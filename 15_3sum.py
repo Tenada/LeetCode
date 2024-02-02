590987ee0fd76d359def466e15bdfd8d164dd177
Shchelcunov <shchelcunov@gmail.com>
2024-02-02 12:46:11 +0300

    №15 solved, passed 308/312 testcases.

--- 15_3sum.py
+++ 15_3sum.py
@@ -7,26 +7,15 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         all_triplets = combinations(nums, 3)
-        zero_triplets = [list(triplet) for triplet in all_triplets if sum(triplet)==0]
+        zero_triplets = {tuple(sorted(triplet)) for triplet in all_triplets if sum(triplet)==0}
+        distinct_triplets = [list(triplet) for triplet in zero_triplets]
 
-        if not zero_triplets: # Empty list is false.
-            return zero_triplets
-
-        distinct_triplets = []
-
-        # Вариант 1. Проверить, будут ли все комбинации одного списка равны всем комбинациям другого.
-        # Вариант 2. Проверить, найдутся ли числа из одного списка в другом.внутренний списрк нельзя захешировать.
-        # Напомню, что
-        # [[-1, 0, 1], [-1, 2, -1], [0, 1, -1]]
-
-        for triplet in zero_triplets:
-            distinct_triplets.append(triplet)
-
+        # Empty list is false.
         return distinct_triplets
 
 
 solution = Solution()
 nums = [-1, 0, 1, 2, -1, -4]
-#nums = [0, 1, 1]
+#nums = [0, 0, 0, 0]
 
 print(solution.threeSum(nums))
