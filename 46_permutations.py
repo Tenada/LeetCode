a26ac9e1cac11c9855b5c80fdfad90e8d3feb90a
Shchelcunov <shchelcunov@gmail.com>
2024-02-25 11:16:08 +0300

    Removed all the solved single files.

--- 46_permutations.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-class Solution(object):
-    def permute(self, nums):
-        return list(permutations(nums, r = None))
-
-nums = [1, 3, 5, 8]
-solution = Solution()
-print(solution.permute(nums))
--- 9_palindrome_number.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        if str(x) == str(x)[::-1]:
-            return True
-        else:
-            return False
-
-solution = Solution()
-print(solution.isPalindrome(1212332221))
